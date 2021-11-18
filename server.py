57559d6a36b27da5bf05962d9ad9f71fb8afcc68
wilson02 <bhaddhadon@gmail.com>
2021-11-18 14:53:10 +0000

    adjust server2

--- server.py
+++ server.py
@@ -13,13 +13,13 @@ rec_dict = pickle.load(open('./trg/prod_rec_dict.pkl','rb'))
 @app.route('/api',methods=['POST'])
 def predict():
     # Get the data from the POST request.
-	data = request.get_json(force=True)
+	data = request.get_json(force=True)['feature']
 	age_grp = formatting.age_grp(data[0])
 	gender = data[1]
 	region = 'region_'+str(data[2])
 	key = (age_grp, gender, region)
 	predict = rec_dict[key]
-	
+
 	return jsonify(predict)
 
 if __name__ == '__main__':
